2fe285c83cdefd11aacd3c5236c65b1f04cd05ed
Andrew Dummer <andrewdummer@Andrews-MacBook-Pro.local>
2020-05-03 19:54:21 -0700

    correction

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,15 +7,14 @@ class Quiz extends Component{
     constructor(props){
         super(props)
             this.state = {quiz_position: 1};
-        }
-
-        render(){
-            return(
-                 <div>Quiz</div>
-                 )
-        }
     }
 
+    render(){
+    return(
+            <div>Quiz</div>
+            )
+    }
 }
+
 
 export default Quiz

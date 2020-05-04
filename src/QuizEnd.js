e00b479bb7658d63e2f7aa8b62113e0f49b508ee
Andrew Dummer <andrewdummer@Andrews-MacBook-Pro.local>
2020-05-03 22:01:27 -0700

    fdsaf

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 class QuizEnd extends Component {
     
     render(){
-        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position - 1;
 
-        if 
         return(<div>
         <p>Thanks for playing!</p>
         <a href=''>Reset Quiz</a>
